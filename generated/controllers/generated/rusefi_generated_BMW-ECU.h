49b84ae25cb749a8dc14218ea198595044a645f1
GitHub git update Action <action@github.com>
2024-06-19 16:49:22 +0000

    Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_BMW-ECU.h
+++ generated/controllers/generated/rusefi_generated_BMW-ECU.h
@@ -1274,7 +1274,7 @@
 #define show_microRusEFI_presets false
 #define show_Proteus_presets false
 #define show_test_presets true
-#define SIGNATURE_HASH 1251071004
+#define SIGNATURE_HASH 1168915657
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1667,7 +1667,7 @@
 #define ts_show_vr_threshold_all true
 #define ts_show_vr_threshold_pins true
 #define ts_show_vvt_output true
-#define TS_SIGNATURE "rusEFI main.2024.06.18.BMW-ECU.1251071004"
+#define TS_SIGNATURE "rusEFI main.2024.06.19.BMW-ECU.1168915657"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_BMW-ECU.h
+++ generated/controllers/generated/signature_BMW-ECU.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1251071004
-#define TS_SIGNATURE "rusEFI main.2024.06.18.BMW-ECU.1251071004"
+#define SIGNATURE_HASH 1168915657
+#define TS_SIGNATURE "rusEFI main.2024.06.19.BMW-ECU.1168915657"
