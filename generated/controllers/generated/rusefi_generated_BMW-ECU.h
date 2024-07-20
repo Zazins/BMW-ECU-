b71ebac7bfa5a61fad9251cd59ff65eb0a86a6bf
GitHub git update Action <action@github.com>
2024-07-20 16:46:41 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_BMW-ECU.h
+++ generated/controllers/generated/rusefi_generated_BMW-ECU.h
@@ -1707,7 +1707,7 @@
 #define ts_show_vvt_output true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.07.19.BMW-ECU.25815703"
+#define TS_SIGNATURE "rusEFI main.2024.07.20.BMW-ECU.25815703"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
