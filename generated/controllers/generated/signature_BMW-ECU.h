84728120dfbcc76c1c643c107617a75a0d664c95
GitHub git update Action <action@github.com>
2024-08-03 16:47:07 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_BMW-ECU.h
+++ generated/controllers/generated/signature_BMW-ECU.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3044210534
-#define TS_SIGNATURE "rusEFI main.2024.08.02.BMW-ECU.3044210534"
+#define SIGNATURE_HASH 2563999413
+#define TS_SIGNATURE "rusEFI main.2024.08.03.BMW-ECU.2563999413"
